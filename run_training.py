f153a0c614e1cefb31669edd91674abbc172d052
Piotr Niewinski <me@piotrniewinski.com>
2023-03-14 22:57:54 +0100

    upd

--- run_training.py
+++ run_training.py
@@ -15,156 +15,6 @@ from envies import SimpleBoardGame, CartPoleEnvy, AcrobotEnvy
 
 
 
-train_configs = {
-
-    'SBG_QTable': {
-        'envy_type':        SimpleBoardGame,
-        'envy_point':       {'board_size':6},
-        'actor_type':       QTableActor,
-        'actor_point':      {},
-        'trainer_type':     QTableTrainer ,
-        'trainer_point':    {
-            'batch_size':       10,
-            'mem_batches':      10,
-            'exploration':      0.5,
-            'train_sampled':    0.1,
-            'gamma':            0.5,
-            'update_rate':      0.5},
-        'num_updates':      100,
-        'test_freq':        10},
-
-    'SBG_DQN': {
-        'envy_type':        SimpleBoardGame,
-        'envy_point':       {'board_size':6},
-        'actor_type':       DQNActor,
-        'actor_point':      {
-            'baseLR':           0.01,
-            'device':           None},
-        'trainer_type':     QLearningTrainer,
-        'trainer_point':    {
-            'batch_size':       10,
-            'mem_batches':      10,
-            'exploration':      0.5,
-            'train_sampled':    0.3,
-            'gamma':            0.5},
-        'num_updates':      200,
-        'test_freq':        10},
-
-    'CP_PG': {
-        'envy_type':        CartPoleEnvy,
-        'envy_point':       {
-            'reward_scale': 0.1,
-            'lost_penalty': -10.0},
-        'actor_type':       PGActor,
-        'actor_point':      {
-            'hidden_layers':    (20,20),
-            'baseLR':           0.001,# if not episodic else 0.01,
-            'lay_norm':         True,
-            'use_scaled_ce':    False,  # TODO: check, but with lower LR
-            'do_clip':          True},
-        'trainer_type':     PGTrainer,
-        'trainer_point':    {
-            'batch_size':       256,# if not episodic else 555,
-            'exploration':      0.1,
-            'train_sampled':    0.3,
-            'discount':         0.98,
-            'use_mavg':         True,
-            'mavg_factor':      0.3,
-            'do_zscore':        True},
-        'num_updates':      1000,
-        'upd_on_episode':   False,#episodic,
-        'test_freq':        50,
-        'test_episodes':    10},
-
-    'CP_AC': {
-        'envy_type':        CartPoleEnvy,
-        'envy_point':       {
-            'reward_scale': 0.1,
-            'lost_penalty': -10.0},
-        'actor_type':       PGActor,
-        'actor_point':      {
-            'hidden_layers':    (20,20),
-            'baseLR':           0.0005},
-        'trainer_type':     ACTrainer,
-        'trainer_point':    {
-            'critic_class':     ACCritic,
-            'critic_gamma':     0.99,
-            'critic_baseLR':    0.0005,
-            'batch_size':       256,
-            'exploration':      0.1,
-            'train_sampled':    0.0,
-            'discount':         0.98,
-            'use_mavg':         False,
-            'mavg_factor':      0.3,
-            'do_zscore':        False},
-        'num_updates':      1000,
-        'upd_on_episode':   False,
-        'test_freq':        50,
-        'test_episodes':    10},
-
-    'CP_A2C': {
-        'envy_type':        CartPoleEnvy,
-        'envy_point':       {
-            'reward_scale': 0.1,
-            'won_reward':   0.0,
-            'lost_penalty': -1.0,
-            #'max_steps':    200,
-        },
-        'actor_type':       A2CActor,
-        'actor_point':      {
-            #'devices':          None,
-            'two_towers':       True,   #False,
-            'num_layers':       1,      #2,
-            'layer_width':      50,     #20,
-            'lay_norm':         True,
-            'clamp_advantage':  None,   #0.5,
-            'use_scaled_ce':    True,
-            'use_huber':        True,
-            'baseLR':           0.01,
-            'do_clip':          True,
-        },
-        'trainer_type':     PGTrainer,
-        'trainer_point':    {
-            'batch_size':       256,    #768,
-            'exploration':      0.2,    #0.0,
-            'train_sampled':    0.1,    #0.0,
-            'discount':         0.98,
-            'use_mavg':         True,
-            'mavg_factor':      0.12,
-            'do_zscore':        False},
-        'num_updates':      2000,
-        'upd_on_episode':   False,      #True,
-        'test_freq':        50,
-        'test_episodes':    10,
-        'test_render':      True,
-        'inspect':          True,
-    },
-
-    'ACR_AC': {
-        'envy_type':        AcrobotEnvy,
-        'envy_point':       {},
-        'actor_type':       PGActor,
-        'actor_point':      {'baseLR':0.01},
-        'trainer_type':     ACTrainer,
-        'trainer_point':    {
-            'critic_class':     ACCritic,
-            'critic_gamma':     0.99,
-            'critic_baseLR':    0.1,
-            'batch_size':       500,
-            'exploration':      0.1,
-            'train_sampled':    0.0,
-            'discount':         0.98,
-            'use_mavg':         False,
-            'mavg_factor':      0.3,
-            'do_zscore':        False},
-        'num_updates':      500,
-        'upd_on_episode':   True, # TODO:?
-        'test_freq':        20,
-        'test_episodes':    10},
-
-}
-
-
 def run_actor_training(
         envy_type: type(RLEnvy),
         envy_point: dict,                   # for RLEnvy init
@@ -222,6 +72,155 @@ def run_actor_training(
 
 
 if __name__ == "__main__":
+
+    train_configs = {
+
+        'SBG_QTable': {
+            'envy_type':        SimpleBoardGame,
+            'envy_point':       {'board_size':6},
+            'actor_type':       QTableActor,
+            'actor_point':      {},
+            'trainer_type':     QTableTrainer ,
+            'trainer_point':    {
+                'batch_size':       10,
+                'mem_batches':      10,
+                'exploration':      0.5,
+                'train_sampled':    0.1,
+                'gamma':            0.5,
+                'update_rate':      0.5},
+            'num_updates':      100,
+            'test_freq':        10},
+
+        'SBG_DQN': {
+            'envy_type':        SimpleBoardGame,
+            'envy_point':       {'board_size':6},
+            'actor_type':       DQNActor,
+            'actor_point':      {
+                'baseLR':           0.01,
+                'device':           None},
+            'trainer_type':     QLearningTrainer,
+            'trainer_point':    {
+                'batch_size':       10,
+                'mem_batches':      10,
+                'exploration':      0.5,
+                'train_sampled':    0.3,
+                'gamma':            0.5},
+            'num_updates':      200,
+            'test_freq':        10},
+
+        'CP_PG': {
+            'envy_type':        CartPoleEnvy,
+            'envy_point':       {
+                'reward_scale': 0.1,
+                'lost_penalty': -10.0},
+            'actor_type':       PGActor,
+            'actor_point':      {
+                'hidden_layers':    (20,20),
+                'baseLR':           0.001,# if not episodic else 0.01,
+                'lay_norm':         True,
+                'use_scaled_ce':    False,  # TODO: check, but with lower LR
+                'do_clip':          True},
+            'trainer_type':     PGTrainer,
+            'trainer_point':    {
+                'batch_size':       256,# if not episodic else 555,
+                'exploration':      0.1,
+                'train_sampled':    0.3,
+                'discount':         0.98,
+                'use_mavg':         True,
+                'mavg_factor':      0.3,
+                'do_zscore':        True},
+            'num_updates':      1000,
+            'upd_on_episode':   False,#episodic,
+            'test_freq':        50,
+            'test_episodes':    10},
+
+        'CP_AC': {
+            'envy_type':        CartPoleEnvy,
+            'envy_point':       {
+                'reward_scale': 0.1,
+                'lost_penalty': -10.0},
+            'actor_type':       PGActor,
+            'actor_point':      {
+                'hidden_layers':    (20,20),
+                'baseLR':           0.0005},
+            'trainer_type':     ACTrainer,
+            'trainer_point':    {
+                'critic_class':     ACCritic,
+                'critic_gamma':     0.99,
+                'critic_baseLR':    0.0005,
+                'batch_size':       256,
+                'exploration':      0.1,
+                'train_sampled':    0.0,
+                'discount':         0.98,
+                'use_mavg':         False,
+                'mavg_factor':      0.3,
+                'do_zscore':        False},
+            'num_updates':      1000,
+            'upd_on_episode':   False,
+            'test_freq':        50,
+            'test_episodes':    10},
+
+        'CP_A2C': {
+            'envy_type':        CartPoleEnvy,
+            'envy_point':       {
+                'reward_scale': 0.1,
+                'won_reward':   0.0,
+                'lost_penalty': -1.0,
+                #'max_steps':    200,
+            },
+            'actor_type':       A2CActor,
+            'actor_point':      {
+                #'devices':          None,
+                'two_towers':       True,   #False,
+                'num_layers':       1,      #2,
+                'layer_width':      50,     #20,
+                'lay_norm':         True,
+                'clamp_advantage':  None,   #0.5,
+                'use_scaled_ce':    True,
+                'use_huber':        True,
+                'baseLR':           0.01,
+                'do_clip':          True,
+            },
+            'trainer_type':     PGTrainer,
+            'trainer_point':    {
+                'batch_size':       256,    #768,
+                'exploration':      0.2,    #0.0,
+                'train_sampled':    0.1,    #0.0,
+                'discount':         0.98,
+                'use_mavg':         True,
+                'mavg_factor':      0.12,
+                'do_zscore':        False},
+            'num_updates':      2000,
+            'upd_on_episode':   False,      #True,
+            'test_freq':        50,
+            'test_episodes':    10,
+            'test_render':      True,
+            'inspect':          True,
+        },
+
+        'ACR_AC': {
+            'envy_type':        AcrobotEnvy,
+            'envy_point':       {},
+            'actor_type':       PGActor,
+            'actor_point':      {'baseLR':0.01},
+            'trainer_type':     ACTrainer,
+            'trainer_point':    {
+                'critic_class':     ACCritic,
+                'critic_gamma':     0.99,
+                'critic_baseLR':    0.1,
+                'batch_size':       500,
+                'exploration':      0.1,
+                'train_sampled':    0.0,
+                'discount':         0.98,
+                'use_mavg':         False,
+                'mavg_factor':      0.3,
+                'do_zscore':        False},
+            'num_updates':      500,
+            'upd_on_episode':   True, # TODO:?
+            'test_freq':        20,
+            'test_episodes':    10},
+
+    }
 
     for config_name in [
         #'SBG_QTable',
