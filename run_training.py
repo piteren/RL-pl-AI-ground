bd48824f81c6b8b250b58bfe2849cdd05590428e
Piotr Niewinski <me@piotrniewinski.com>
2023-03-15 23:12:09 +0100

    trainers -> one runner, actor update split

--- run_training.py
+++ run_training.py
@@ -1,14 +1,11 @@
 from r4c.envy import RLEnvy
 from r4c.actor import TrainableActor
-from r4c.trainer import RLTrainer
+from r4c.runner import RLRunner
 from r4c.qlearning.qtable.qt_actor import QTableActor
-from r4c.qlearning.qtable.qt_trainer import QTableTrainer
 from r4c.qlearning.dqn.dqn_actor import DQNActor
-from r4c.qlearning.ql_trainer import QLearningTrainer
 from r4c.policy_gradients.pg_actor import PGActor
-from r4c.policy_gradients.pg_trainer import PGTrainer
+from r4c.policy_gradients.actor_critic.ac_actor import ACActor
 from r4c.policy_gradients.actor_critic.ac_critic import ACCritic
-from r4c.policy_gradients.actor_critic.ac_trainer import ACTrainer
 from r4c.policy_gradients.a2c.a2c_actor import A2CActor
 
 from envies import SimpleBoardGame, CartPoleEnvy, AcrobotEnvy
@@ -20,8 +17,6 @@ def run_actor_training(
         envy_point: dict,                   # for RLEnvy init
         actor_type: type(TrainableActor),
         actor_point: dict,                  # for TrainableActor init
-        trainer_type: type(RLTrainer),
-        trainer_point: dict,                # for RLTrainer init
         nTS_ep=         100,
         seed=           121,
         loglevel=       20,
@@ -45,18 +40,17 @@ def run_actor_training(
         loglevel=   loglevel,
         **actor_point)
 
-    trainer = trainer_type(
+    runner = RLRunner(
         envy=       envy,
         actor=      actor,
         seed=       seed,
-        loglevel=   loglevel,
-        **trainer_point)
+        loglevel=   loglevel)
 
     if nTS_ep:
-        ts_res = trainer.test_on_episodes(n_episodes=nTS_ep)
+        ts_res = runner.test_on_episodes(n_episodes=nTS_ep)
         print(f'Test report: won factor: {int(ts_res[0]*100)}%, avg reward: {ts_res[1]:.1f}')
 
-    tr_res = trainer.train(**train_kwargs)
+    tr_res = runner.train(**train_kwargs)
     if not hpmser_mode:
         print('Training report:')
         print(f'> number of actions performed (n_actions):                    {tr_res["n_actions"]}')
@@ -65,7 +59,7 @@ def run_actor_training(
         print(f'> max number of succeeded tests in a row (succeeded_row_max): {tr_res["succeeded_row_max"]}')
 
     if nTS_ep:
-        ts_res = trainer.test_on_episodes(n_episodes=nTS_ep)
+        ts_res = runner.test_on_episodes(n_episodes=nTS_ep)
         print(f'Test report: won factor: {int(ts_res[0]*100)}%, avg reward: {ts_res[1]:.1f}')
 
     return tr_res
@@ -79,16 +73,15 @@ if __name__ == "__main__":
             'envy_type':        SimpleBoardGame,
             'envy_point':       {'board_size':6},
             'actor_type':       QTableActor,
-            'actor_point':      {},
-            'trainer_type':     QTableTrainer ,
-            'trainer_point':    {
-                'batch_size':       10,
-                'mem_batches':      10,
-                'exploration':      0.5,
-                'train_sampled':    0.1,
+            'actor_point':      {
                 'gamma':            0.5,
                 'update_rate':      0.5},
             'num_updates':      100,
+            'batch_size':       10,
+            'mem_batches':      10,
+            'sample_memory':    True,
+            'exploration':      0.5,
+            'sampled_TR':       0.1,
             'test_freq':        10},
 
         'SBG_DQN': {
@@ -96,16 +89,15 @@ if __name__ == "__main__":
             'envy_point':       {'board_size':6},
             'actor_type':       DQNActor,
             'actor_point':      {
+                'gamma':            0.5,
                 'baseLR':           0.01,
                 'device':           None},
-            'trainer_type':     QLearningTrainer,
-            'trainer_point':    {
-                'batch_size':       10,
-                'mem_batches':      10,
-                'exploration':      0.5,
-                'train_sampled':    0.3,
-                'gamma':            0.5},
             'num_updates':      200,
+            'batch_size':       10,
+            'mem_batches':      10,
+            'sample_memory':    True,
+            'exploration':      0.5,
+            'sampled_TR':       0.3,
             'test_freq':        10},
 
         'CP_PG': {
@@ -115,21 +107,19 @@ if __name__ == "__main__":
                 'lost_penalty': -10.0},
             'actor_type':       PGActor,
             'actor_point':      {
+                'discount':         0.98,
+                'use_mavg':         True,
+                'mavg_factor':      0.3,
+                'do_zscore':        True,
                 'hidden_layers':    (20,20),
                 'baseLR':           0.001,# if not episodic else 0.01,
                 'lay_norm':         True,
                 'use_scaled_ce':    False,  # TODO: check, but with lower LR
                 'do_clip':          True},
-            'trainer_type':     PGTrainer,
-            'trainer_point':    {
-                'batch_size':       256,# if not episodic else 555,
-                'exploration':      0.1,
-                'train_sampled':    0.3,
-                'discount':         0.98,
-                'use_mavg':         True,
-                'mavg_factor':      0.3,
-                'do_zscore':        True},
             'num_updates':      1000,
+            'batch_size':       256,  # if not episodic else 555,
+            'exploration':      0.1,
+            'sampled_TR':       0.3,
             'upd_on_episode':   False,#episodic,
             'test_freq':        50,
             'test_episodes':    10},
@@ -139,23 +129,21 @@ if __name__ == "__main__":
             'envy_point':       {
                 'reward_scale': 0.1,
                 'lost_penalty': -10.0},
-            'actor_type':       PGActor,
+            'actor_type':       ACActor,
             'actor_point':      {
-                'hidden_layers':    (20,20),
-                'baseLR':           0.0005},
-            'trainer_type':     ACTrainer,
-            'trainer_point':    {
-                'critic_class':     ACCritic,
-                'critic_gamma':     0.99,
-                'critic_baseLR':    0.0005,
-                'batch_size':       256,
-                'exploration':      0.1,
-                'train_sampled':    0.0,
                 'discount':         0.98,
                 'use_mavg':         False,
                 'mavg_factor':      0.3,
-                'do_zscore':        False},
+                'do_zscore':        False,
+                'critic_class':     ACCritic,
+                'critic_gamma':     0.99,
+                'critic_baseLR':    0.0005,
+                'hidden_layers':    (20,20),
+                'baseLR':           0.0005},
             'num_updates':      1000,
+            'batch_size':       256,
+            'exploration':      0.1,
+            'sampled_TR':       0.0,
             'upd_on_episode':   False,
             'test_freq':        50,
             'test_episodes':    10},
@@ -170,7 +158,10 @@ if __name__ == "__main__":
             },
             'actor_type':       A2CActor,
             'actor_point':      {
-                #'devices':          None,
+                'discount':         0.98,
+                'use_mavg':         True,
+                'mavg_factor':      0.12,
+                'do_zscore':        False,
                 'two_towers':       True,   #False,
                 'num_layers':       1,      #2,
                 'layer_width':      50,     #20,
@@ -179,22 +170,14 @@ if __name__ == "__main__":
                 'use_scaled_ce':    True,
                 'use_huber':        True,
                 'baseLR':           0.01,
-                'do_clip':          True,
-            },
-            'trainer_type':     PGTrainer,
-            'trainer_point':    {
-                'batch_size':       256,    #768,
-                'exploration':      0.2,    #0.0,
-                'train_sampled':    0.1,    #0.0,
-                'discount':         0.98,
-                'use_mavg':         True,
-                'mavg_factor':      0.12,
-                'do_zscore':        False},
+                'do_clip':          True},
             'num_updates':      2000,
+            'batch_size':       256,  # 768,
+            'exploration':      0.2,  # 0.0,
+            'sampled_TR':       0.1,  # 0.0,
             'upd_on_episode':   False,      #True,
             'test_freq':        50,
             'test_episodes':    10,
-            'test_render':      True,
             'inspect':          True,
         },
 
@@ -203,7 +186,6 @@ if __name__ == "__main__":
             'envy_point':       {},
             'actor_type':       PGActor,
             'actor_point':      {'baseLR':0.01},
-            'trainer_type':     ACTrainer,
             'trainer_point':    {
                 'critic_class':     ACCritic,
                 'critic_gamma':     0.99,
@@ -226,8 +208,8 @@ if __name__ == "__main__":
         #'SBG_QTable',
         #'SBG_DQN',
         #'CP_PG',
-        'CP_AC',
-        #'CP_A2C',
+        #'CP_AC',
+        'CP_A2C',
         #'ACR_AC',
     ]:
         run_actor_training(
