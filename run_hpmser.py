0f1133b1a9caa5caf7d2fdfabeb8329161ec1897
Piotr Niewinski <me@piotrniewinski.com>
2023-04-01 16:39:56 +0200

    updated hpmser run script

--- run_hpmser.py
+++ run_hpmser.py
@@ -41,11 +41,11 @@ def run_actor_training_wrap(
 
     tr_res = run_actor_training(**pd)
 
-    if tr_res['n_updates_done'] == pd['num_updates']:
+    if tr_res['n_updates_done'] == pd['num_batches']:
         return 0.0
 
-    num_actions_done = tr_res['n_updates_done'] * pd['batch_size']
-    num_actions_max = pd['num_updates'] * max_batch_size
+    num_actions_done = tr_res['n_updates_done'] * actor_point['batch_size']
+    num_actions_max = pd['num_batches'] * max_batch_size
 
     return (num_actions_max - num_actions_done) / num_actions_max
 
@@ -57,43 +57,59 @@ if __name__ == "__main__":
 
         'DQN_CP': {
             'psdd': {
-                'act_gamma':            [0.5,0.99],
+                'act_exploration':      [0.0,0.9],
+                'act_sampled_TR':       [0.0,0.9],
+                'act_batch_size':       (8,16,32,64,128,256),
+                'act_mem_batches':      [1,20],
+                'act_gamma':            [0.5, 0.99],
                 #'act_lay_norm':         (True, False),
                 #'act_use_huber':        (True, False),
                 #'act_do_clip':          (True, False),
                 'mot_n_hidden':         [1,3],
                 'mot_hidden_width':     [10,50],
                 'mot_baseLR':           (0.03, 0.01, 0.005, 0.001, 0.0005, 0.0001),
-                'batch_size':           (8,16,32,64,128,256),
-                'mem_batches':          [1,20],
-                'exploration':          [0.0,0.9],
-                'sampled_TR':           [0.0,0.9],
             },
         },
 
         'PG_CP': {
             'psdd': {
+                'act_exploration':      [0.0,1.0],
+                'act_sampled_TR':       [0.0,1.0],
+                'act_batch_size':       (8,16,32,64,128,256),
                 'act_discount':         [0.5,1.0],
                 'mot_n_hidden':         [1,3],
                 'mot_hidden_width':     [10,50],
                 'mot_baseLR':           (0.03, 0.01, 0.005, 0.001, 0.0005, 0.0001),
-                'batch_size':           (8,16,32,64,128,256),
-                'exploration':          [0.0,1.0],
-                'sampled_TR':           [0.0,1.0],
+            }
+        },
+
+        'AC_CP': {
+            'psdd': {
+                'act_exploration':          [0.0,1.0],
+                'act_sampled_TR':           [0.0,1.0],
+                'act_batch_size':           (8,16,32,64,128,256),
+                'act_discount':             [0.5,1.0],
+                'act_critic_gamma':         [0.0,1.0],
+                'act_critic_baseLR':        (0.03, 0.01, 0.005, 0.001, 0.0005, 0.0001),
+                'act_critic_n_hidden':      [1,3],
+                'act_critic_hidden_width':  [10,50],
+                'mot_n_hidden':             [1,3],
+                'mot_hidden_width':         [10,50],
+                'mot_baseLR':               (0.03, 0.01, 0.005, 0.001, 0.0005, 0.0001),
             }
         },
 
         'A2C_CP': {
             'psdd': {
+                'act_exploration':      [0.0,1.0],
+                'act_sampled_TR':       [0.0,1.0],
+                'act_batch_size':       (8,16,32,64,128,256),
                 'act_discount':         [0.0,1.0],
                 'mot_two_towers':       (True, False),
                 'mot_n_hidden':         [1,3],
                 'mot_hidden_width':     [10,50],
                 'mot_use_huber':        (True, False),
                 'mot_baseLR':           (0.03, 0.01, 0.005, 0.001, 0.0005, 0.0001),
-                'batch_size':           (8,16,32,64,128,256),
-                'exploration':          [0.0,1.0],
-                'sampled_TR':           [0.0,1.0],
             }
         },
 
@@ -102,17 +118,18 @@ if __name__ == "__main__":
     for rc_name in [
         #'DQN_CP',
         #'PG_CP',
-        'A2C_CP',
+        'AC_CP',
+        #'A2C_CP',
     ]:
 
         func_const = {
             'run_config_name':  rc_name,
-            'num_updates':      1000,
+            'num_batches':      1000,
             'test_freq':        20,
             'test_episodes':    10,
             'inspect':          False,
-            'break_ntests':     3,
-            'max_batch_size':   max(hpmser_configs[rc_name]['psdd']['batch_size']),
+            'break_ntests':     1,
+            'max_batch_size':   max(hpmser_configs[rc_name]['psdd']['act_batch_size']),
         }
         if 'const' in hpmser_configs[rc_name]:
             func_const.update(hpmser_configs[rc_name]['const'])
@@ -123,7 +140,7 @@ if __name__ == "__main__":
             func_const= func_const,
             devices=    [None]*10,
             n_loops=    1000,
-            plot_axes=  ['mot_hidden_width','exploration'],
+            plot_axes=  ['mot_hidden_width','act_exploration'],
             #loglevel=   10,
             #do_TB=      False,
         )
